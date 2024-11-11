58ac64e8101b457c5b2ae11ddb6426fc0ae3e284
troyc03 <tchin@django.com>
2024-11-11 00:28:40 -0800

    Updated data_logger.py

--- data_logger.py
+++ data_logger.py
@@ -1,1 +1,10 @@
+"""
+File name: data_logger.py
+Author: Troy Chin (CWID: 885586685)
+Date: 2024-11-10
+Version: 1.0
+Description: This script will handle any data logging from the user.
 
+"""
+
+#Import models here.
