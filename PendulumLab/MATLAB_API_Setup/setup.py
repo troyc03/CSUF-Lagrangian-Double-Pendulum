5e45004142317d81810ae86d1512edb5aa6fc15e
Troy Chin <148022362+troyc03@users.noreply.github.com>
2024-11-15 14:04:04 -0800

    Update setup.py

--- PendulumLab/MATLAB_API_Setup/setup.py
+++ PendulumLab/MATLAB_API_Setup/setup.py
@@ -7,7 +7,21 @@ Purpose: This is the setup file to integrate MATLAB's API Engine to Python.
 
 """
 
-#Import models here.
+import matlab.engine
 
-def setup_engine():
-    pass
+def setup_engine(options='-desktop'):
+    try:
+        eng = matlab.engine.start_matlab(options)
+        print("MATLAB Engine initiated successfully.")
+        return eng
+    except Exception as e:
+        print(f"ERROR: Initiation failed. Details: {e}")
+        return None
+ 
+eng = setup_engine()
+
+if eng:
+    print("MATLAB Engine is running.")
+else:
+    print("Failed to start MATLAB Engine.")
+
