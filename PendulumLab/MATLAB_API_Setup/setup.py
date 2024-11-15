f0475056832d683d0a83087587eb79d00dd665c9
Troy Chin <148022362+troyc03@users.noreply.github.com>
2024-11-15 12:58:01 -0800

    Add files via upload

---
+++ PendulumLab/MATLAB_API_Setup/setup.py
@@ -0,0 +1,13 @@
+"""
+File name: setup.py
+Author: Troy Chin
+Date: 15 November 2024
+Version: 1.0
+Purpose: This is the setup file to integrate MATLAB's API Engine to Python.
+
+"""
+
+#Import models here.
+
+def setup_engine():
+    pass
