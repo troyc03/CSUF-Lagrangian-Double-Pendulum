d4d3f00ecfcf7350f9f7424cee77e6f570fa53eb
troyc03 <tchin@django.com>
2024-11-16 16:13:18 -0800

    Update setup.py

--- PendulumLab/MATLAB_API_Setup/setup.py
+++ PendulumLab/MATLAB_API_Setup/setup.py
@@ -6,27 +6,27 @@ Version: 1.0
 Description: This is a test file to properly integrate MATLAB's API Engine to Python.
 """
 
-try:
-    import matlab.engine
-except ModuleNotFoundError:
-        raise ModuleNotFoundError(
-        "ERROR: The MATLAB Engine API module is not installed. Please ensure the MATLAB Engine API for Python is installed and properly integrated with your Python environment."
-        )
+# try:
+#     # import matlab.engine
+# except ModuleNotFoundError:
+#         raise ModuleNotFoundError(
+#         "ERROR: The MATLAB Engine API module is not installed. Please ensure the MATLAB Engine API for Python is installed and properly integrated with your Python environment."
+#         )
 
-def setup_engine():
-    try:
-        eng = matlab.engine.start_matlab()
-        return eng
-    except Exception as e:
-        raise RuntimeError(
-            f"ERROR: Failed to start the MATLAB Engine. Ensure MATLAB is installed and configured correctly. Details: {e}"
-        )
+# def setup_engine():
+#     try:
+#         eng = matlab.engine.start_matlab()
+#         return eng
+#     except Exception as e:
+#         raise RuntimeError(
+#             f"ERROR: Failed to start the MATLAB Engine. Ensure MATLAB is installed and configured correctly. Details: {e}"
+#         )
 
-# Initialize the MATLAB Engine
-try:
-    boot = setup_engine()
-    print("MATLAB Engine successfully started.")
-except ModuleNotFoundError as e:
-    print(e)
-except RuntimeError as e:
-    print(e)
+# # Initialize the MATLAB Engine
+# try:
+#     boot = setup_engine()
+#     print("MATLAB Engine successfully started.")
+# except ModuleNotFoundError as e:
+#     print(e)
+# except RuntimeError as e:
+#     print(e)
